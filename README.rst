ff264e0678d195d70b114a446165dd92a006d543
murat-polat <muratsdepo@gmail.com>
2020-11-24 16:24:23 +0100

    firstCommit

---
+++ README.rst
@@ -0,0 +1,22 @@
+account plugin for `Tutor <https://docs.tutor.overhang.io>`__
+===================================================================================
+
+Installation
+------------
+
+::
+
+    pip install git+https://github.com/myusername/tutor-account
+
+Usage
+-----
+
+::
+
+    tutor plugins enable account
+    
+
+License
+-------
+
+This software is licensed under the terms of the AGPLv3.
---
+++ setup.py
@@ -0,0 +1,52 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, "README.rst"), "rt", encoding="utf8") as f:
+    readme = f.read()
+
+about = {}
+with io.open(
+    os.path.join(here, "tutoraccount", "__about__.py"),
+    "rt",
+    encoding="utf-8",
+) as f:
+    exec(f.read(), about)
+
+setup(
+    name="tutor-account",
+    version=about["__version__"],
+    url="https://github.com/myusername/tutor-account",
+    project_urls={
+        "Code": "https://github.com/myusername/tutor-account",
+        "Issue tracker": "https://github.com/myusername/tutor-account/issues",
+    },
+    license="AGPLv3",
+    author="MP",
+    description="account plugin for Tutor",
+    long_description=readme,
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    python_requires=">=3.5",
+    install_requires=["tutor-openedx"],
+    entry_points={
+        "tutor.plugin.v0": [
+            "account = tutoraccount.plugin"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
